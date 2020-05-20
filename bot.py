3ebfab0243751afaf16d003be0026e079ee77401
mayur2226 <54096516+mayur2226@users.noreply.github.com>
2020-05-20 11:54:16 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,56 @@
+import requests
+from datetime import datetime
+import json
+class ScoreGet:
+    def __init__(self):
+
+        self.url_get_all_matches = "http://cricapi.com/api/matches"
+        self.url_get_score="http://cricapi.com/api/cricketScore"
+        self.api_key = ""
+        self.unique_id = ""  # unique to every match
+
+    def get_unique_id(self):
+        
+        uri_params = {"apikey": self.api_key}
+        resp = requests.get(self.url_get_all_matches, params=uri_params)
+        resp_dict = resp.json()
+        uid_found=0
+        for i in resp_dict['matches']:
+            if (i['team-1'] == "India" or i['team-2'] == "India"):
+                todays_date = datetime.today().strftime('%Y-%m-%d')
+                if todays_date == i['date'].split("T")[0]:
+                    uid_found=1
+                    self.unique_id=i['unique_id']
+                    print(self.unique_id)
+                    break
+        if not uid_found:
+            self.unique_id=-1
+
+        send_data=self.get_score(self.unique_id)
+        return send_data
+    def get_score(self,unique_id):
+        data="" 
+        if unique_id == -1:
+            data="No India matches today"
+        else:
+            uri_params = {"apikey": self.api_key, "unique_id": self.unique_id}
+            resp=requests.get(self.url_get_score,params=uri_params)
+            data_json=resp.json()
+            
+            try:
+                data="Here's the score : "+ "\n" + data_json['stat'] +'\n' + data_json['score']
+            except KeyError as e:
+                data="Something went wrong"
+        return data
+
+
+
+if __name__ == "__main__":
+    match_obj=ScoreGet()
+    send_message=match_obj.get_unique_id()
+    print(send_message)
+    from twilio.rest import Client
+    account_sid = ''
+    auth_token = ''
+    client = Client(account_sid, auth_token)
+    message = client.messages.create( body=send_message, from_='whatsapp:', to='whatsapp:' )
